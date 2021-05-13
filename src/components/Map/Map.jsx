051029ef64dec3a5dd75657c7ab2cadae1638e2a
Oğuzhan Uzunay <C9010278@arcelik.com>
2021-05-13 06:45:32 +0300

    panTo and calulating distance function added to startRide component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,6 +4,7 @@ import Loading from '../Loading/Loading.jsx';
 import { Button, Card, Col, Container, Form, Row } from 'react-bootstrap';
 import '../StartRide/StartRide.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Draggable from 'react-draggable';
 
 import {
   GoogleMap,
@@ -75,6 +76,8 @@ const Map = () => {
   const [infoText, setInfoText] = React.useState('');
   // Hooks
   const [startRide, setStartRide] = useState(true);
+  const [cordinate1, setCordinate1] = useState({ x: 0, y: 0 });
+  const [cordinate2, setCordinate2] = useState({ x: 0, y: 0 });
 
   // development
 
@@ -82,7 +85,23 @@ const Map = () => {
     setStartRide(false);
   };
 
-  const calculateMiddlePoint = (lat1, lng1, lat2, lng2) => {};
+  const calculateDistanceWithEuclidean = (cord1, cord2) => {
+    let distance = Math.hypot(cord1.x - cord2.x, cord1.y - cord2.y);
+    console.log('distance :', distance * 100);
+    let zoomLevel = 4;
+    return distance;
+  };
+
+  const calculateMiddlePoint = (cord1, cord2) => {
+    let distanceX = cord1.x + cord2.x;
+    let distanceY = cord1.y + cord2.y;
+
+    let middlePointX = distanceX / 2;
+    let middlePointY = distanceY / 2;
+    console.log(`Orta Nokta \nX: ${middlePointX} \nY: ${middlePointY}`);
+    calculateDistanceWithEuclidean(cord1, cord2);
+    return [middlePointX, middlePointY];
+  };
 
   // setMarkers((current) => [...current, cordinateList]);
   //console.log(cordinateList);
@@ -117,8 +136,58 @@ const Map = () => {
 
   const panTo = React.useCallback(({ lat, lng }) => {
     mapRef.current.panTo({ lat, lng });
-    mapRef.current.setZoom(14);
+    mapRef.current.setZoom(10);
   });
+
+  const zoomControl = React.useCallback((km) => {
+    /*
+20 : 1128.497220
+19 : 2256.994440
+18 : 4513.988880
+17 : 9027.977761
+16 : 18055.955520
+15 : 36111.911040
+14 : 72223.822090
+13 : 144447.644200
+12 : 288895.288400
+11 : 577790.576700
+10 : 1155581.153000
+9  : 2311162.307000
+8  : 4622324.614000
+7  : 9244649.227000
+6  : 18489298.450000
+5  : 36978596.910000
+4  : 73957193.820000
+3  : 147914387.600000
+2  : 295828775.300000
+1  : 591657550.500000
+*/
+    km = km * 1128;
+    console.log(km);
+    let distance = (591657550 - km) / 6027500;
+    console.log(distance/2);
+    let zoom = Math.pow(distance, 1 / 2);
+
+    console.log(zoom);
+    mapRef.current.setZoom(zoom);
+  });
+
+  /*
+  const getCordinateX = async (result) => {
+    const { lat, lng } = await getLatLng(result[0]);
+    console.log('X', lat);
+    return lat;
+  };
+  const getCordinateY = async (result) => {
+    const { lat, lng } = await getLatLng(result[0]);
+    console.log('Y', lng);
+    return lng;
+  };
+  */
+
+  const updateCord1 = (x, y) => {
+    setCordinate1();
+  };
 
   if (loadError) return 'Error loading Maps';
   if (!isLoaded) return <Loading />;
@@ -126,57 +195,84 @@ const Map = () => {
   return (
     <div>
       {startRide ? (
-        <Card
-          className="card"
-          style={{
-            opacity: 0.85,
-            borderColor: '#603bbb !important',
-            position: 'absolute',
-            left: '50%',
-            top: '50%',
-            transform: 'translate(-50%, -50%)',
-            zIndex: '1',
-            border: 'solid 5px #603bbb',
-          }}
-        >
-          <Card.Header>
-            <h1>Start Ride</h1>
-          </Card.Header>
-          <Card.Body className="m2">
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Choose a starting location.</Form.Label>
+        <Draggable>
+          <Card
+            className="card"
+            style={{
+              opacity: 0.85,
+              borderColor: '#603bbb !important',
+              position: 'absolute',
+              left: '50%',
+              top: '50%',
+              transform: 'translate(-50%, -50%)',
+              zIndex: '1',
+              border: 'solid 5px #603bbb',
+            }}
+          >
+            <Card.Header>
+              <h1>Start Ride</h1>
+            </Card.Header>
+            <Card.Body className="m2">
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Choose a starting location.</Form.Label>
 
-              <Row>
-                <Col sm={10}>
-                  <Search panTo={panTo} />
-                </Col>
-                <Col sm={2} style={{ padding: '0px' }}>
-                  <Locate panTo={panTo} />
-                </Col>
-              </Row>
+                <Row>
+                  <Col sm={10}>
+                    <Search
+                      panTo={panTo}
+                      type={'startPoint'}
+                      setCordinate1={setCordinate1}
+                      setCordinate2={setCordinate2}
+                    />
+                  </Col>
+                  <Col sm={2} style={{ padding: '0px' }}>
+                    <Locate panTo={panTo} setCordinate1={setCordinate1} />
+                  </Col>
+                </Row>
 
-              <Form.Text className="text-muted">
-                Choose your starting location for driving
-              </Form.Text>
-            </Form.Group>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Choose destination location</Form.Label>
-              <Search style={{ margin: '20px', top: '3rem' }} />
-              <Form.Text className="text-muted">
-                Choose your destination for driving{' '}
-              </Form.Text>
-            </Form.Group>
-            <Button
-              variant="primary"
-              style={{ backgroundColor: '#603bbb', borderColor: '#603bbb' }}
-              type="submit"
-              size="lg"
-              block
-            >
-              Submit
-            </Button>
-          </Card.Body>
-        </Card>
+                <Form.Text className="text-muted">
+                  Choose your starting location for driving
+                </Form.Text>
+              </Form.Group>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Choose destination location</Form.Label>
+                <Search
+                  style={{ margin: '20px', top: '3rem' }}
+                  type={'endPoint'}
+                  panTo={panTo}
+                  setCordinate1={setCordinate1}
+                  setCordinate2={setCordinate2}
+                  /*
+                  getCordinateX={getCordinateX}
+                  getCordinateY={getCordinateY}
+                  */
+                />
+                <Form.Text className="text-muted">
+                  Choose your destination for driving{' '}
+                </Form.Text>
+              </Form.Group>
+              <Button
+                variant="primary"
+                style={{ backgroundColor: '#603bbb', borderColor: '#603bbb' }}
+                type="submit"
+                size="lg"
+                type="calculateDistance"
+                block
+                onClick={() => {
+                  panTo({
+                    lat: calculateMiddlePoint(cordinate1, cordinate2)[0],
+                    lng: calculateMiddlePoint(cordinate1, cordinate2)[1],
+                  });
+                  zoomControl(
+                    calculateDistanceWithEuclidean(cordinate1, cordinate2)
+                  );
+                }}
+              >
+                Submit
+              </Button>
+            </Card.Body>
+          </Card>
+        </Draggable>
       ) : (
         false
       )}
@@ -226,7 +322,7 @@ const Map = () => {
   );
 };
 
-function Locate({ panTo }) {
+function Locate({ panTo, setCordinate1 }) {
   return (
     <button
       className="locate"
@@ -236,6 +332,10 @@ function Locate({ panTo }) {
             panTo({
               lat: position.coords.latitude,
               lng: position.coords.longitude,
+            });
+            setCordinate1({
+              x: position.coords.latitude,
+              y: position.coords.longitude,
             });
           },
           () => null
@@ -247,7 +347,7 @@ function Locate({ panTo }) {
   );
 }
 
-function Search({ panTo }) {
+function Search({ panTo, setCordinate1, setCordinate2, type }) {
   const {
     ready,
     value,
@@ -271,11 +371,16 @@ function Search({ panTo }) {
           try {
             const result = await getGeocode({ address });
             const { lat, lng } = await getLatLng(result[0]);
+            if (type === 'startPoint') {
+              setCordinate1({ x: lat, y: lng });
+            } else {
+              setCordinate2({ x: lat, y: lng });
+            }
             panTo({ lat, lng });
             //  console.log(lat, lng);
             // console.log(result[0]);
           } catch (error) {
-            console.log('error!');
+            console.log(error);
           }
         }}
       >
